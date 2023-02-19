11751c27c5e8036bf39806a4052e3e1476eba106
Jugal <jugalchap123@gmail.com>
2023-02-19 16:02:53 +0530

    New change to Events

--- lib/database/Event.dart
+++ lib/database/Event.dart
@@ -3,32 +3,37 @@ import 'package:path/path.dart' as p;
 class Event {
   String title;
   String description = '';
+  String time;
   String creator;
 
   Event({
     required this.title,
     required this.description,
     required this.creator,
+    required this.time,
   });
 }
 
 class SingularEvent extends Event {
   String date;
-  String time;
   SingularEvent(
       {required super.title,
       required super.description,
       required super.creator,
       required this.date,
-      required this.time});
+      required super.time});
 }
 
 class RecurringEvent extends Event {
   String endDate, startDate;
+  int mask;
+
   RecurringEvent(
-      {required super.title,
+      {required super.time,
+      required super.title,
       required super.creator,
       required super.description,
       required this.startDate,
-      required this.endDate});
+      required this.endDate,
+      required this.mask});
 }
