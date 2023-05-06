c1c8b859e2cc69ac4b73a37d6ef08cb738e1c45d
Jatin <75074656+jatin1322@users.noreply.github.com>
2023-05-06 13:03:47 +0530

    Faculty Homepage UI v1

--- lib/views/homePage/faculty_home.dart
+++ lib/views/homePage/faculty_home.dart
@@ -16,9 +16,70 @@ class FacultyHome extends AbstractHome {
 }
 
 class _FacultyHomeState extends AbstractHomeState {
+  Widget allCourses() {
+    List<dynamic> coursesList = f.courses.toList();
+    if (coursesList.isEmpty) {
+      return const Text('No Courses Registered');
+    }
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+        border: Border.all(
+          color: const Color(0xff555555),
+        ),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            blurStyle: BlurStyle.outer,
+            color: Color(primaryLight),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Text(
+              "Courses",
+              style: TextStyle(
+                  color: Color(primaryLight),
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(
+            height: 10,
+          ),
+          SizedBox(
+              height: 150,
+              child: ListView.builder(
+                // shrinkWrap: true,
+                // physics: NeverScrollableScrollPhysics(),
+                itemCount: coursesList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    margin: const EdgeInsets.symmetric(vertical: 4),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.blueGrey,
+                    ),
+                    child: ListTile(
+                      title: Text(coursesList[index],
+                          style: TextStyle(color: Color(secondaryLight)),),
+                    ),
+                  );
+                },
+              )),
+        ],
+      ),
+    );
+  }
+
   @override
   List<Widget> buttons() {
     List<Widget> l = List.empty(growable: true);
+    l.add(allCourses());
     l.add(ElevatedButton(
       onPressed: () {
         Navigator.push(context,
